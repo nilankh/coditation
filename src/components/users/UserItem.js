7cfcd29fb9162c5882514d9111998db1e4e00f6e
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-10-16 00:35:50 +0530

    done iwth users

---
+++ src/components/users/UserItem.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+// const UserItem = (props) => {
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+  // const { login, avatar_url, html_url } = props.user;
+  // u can destructure in line 5 also which i have done same thing as line6
+  return (
+    <div className="card text-center">
+      <img
+        src={avatar_url}
+        alt=""
+        className="round-img"
+        style={{ width: '60px' }}
+      />
+      <h3>{login}</h3>
+
+      <div>
+        <Link to={`/user/${login}`} className="btn btn-dark btn-sm my-1">
+          More
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired,
+};
+
+export default UserItem;
---
+++ src/components/users/Users.js
@@ -0,0 +1,34 @@
+import React, { useContext } from 'react';
+import UserItem from './UserItem';
+import Spinner from '../layout/Spinner';
+import GithubContext from '../../context/github/githubContext';
+
+const Users = () => {
+  const githubContext = useContext(GithubContext);
+
+  const { loading, users } = githubContext;
+
+  // const Users = ({ users, loading }) => {
+  if (loading) {
+    return <Spinner />;
+  } else {
+    return (
+      <div style={userStyle}>
+        {users.map((user) => (
+          //   <div key={user.id}>{user.login}</div>
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    );
+  }
+};
+// Users.propTypes = {
+//   users: PropTypes.array.isRequired,
+//   loading: PropTypes.bool.isRequired,
+// };
+const userStyle = {
+  display: 'grid',
+  gridTemplateColumns: 'repeat(3, 1fr)',
+  gridGap: '1rem',
+};
+export default Users;
